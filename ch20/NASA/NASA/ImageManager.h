29a322c7d3c44be70d12f583488878512dc3e3fe
Rob Napier <robnapier@gmail.com>
2012-07-15 15:36:55 -0400

    Nicely broken version of ZipText

---
+++ ch20/NASA/NASA/ImageManager.h
@@ -0,0 +1,13 @@
+//
+//  ImageManager.h
+//  NASA
+//
+//  Created by Rob Napier on 7/14/12.
+//  Copyright (c) 2012 Rob Napier. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageManager : NSObject
+
+@end
---
+++ ch20/ZipText/ZipText/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  ZipText
+//
+//  Created by Rob Napier on 7/15/12.
+//  Copyright (c) 2012 Rob Napier. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+  if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+      return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+  } else {
+      return YES;
+  }
+}
+
+@end
