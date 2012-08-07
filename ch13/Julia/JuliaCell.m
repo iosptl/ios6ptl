d6d0c7286a9995569cd4f875ecdaafe65a4c9ef2
Rob Napier <robnapier@gmail.com>
2012-08-07 09:40:37 -0400

    Pretty-up the fractals a little

--- ch13/Julia/JuliaCell.m
+++ ch13/Julia/JuliaCell.m
@@ -9,14 +9,13 @@
 #import "JuliaCell.h"
 #import <complex.h>
 
-const double M_PHI = 1.6180339887;
-
-const double blowup = 3;
+//const long double kBlowup = 3; //; 1e2466L; // ~ sqrt(LDBL_MAX)
 
 @interface JuliaCell ()
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @property (weak, nonatomic) IBOutlet UILabel *label;
-@property (nonatomic, readwrite, assign) complex double c;
+@property (nonatomic, readwrite, assign) complex long double c;
+@property (nonatomic, readwrite, assign) complex long double blowup;
 @property (nonatomic, readwrite, assign) NSUInteger rScale;
 @property (nonatomic, readwrite, assign) NSUInteger gScale;
 @property (nonatomic, readwrite, assign) NSUInteger bScale;
@@ -24,7 +23,7 @@ const double blowup = 3;
 
 @implementation JuliaCell
 
-complex double f(complex double z, complex double c) {
+complex long double f(complex long double z, complex long double c) {
   return z*z + c;
 }
 
@@ -41,26 +40,27 @@ complex double f(complex double z, complex double c) {
   NSUInteger components = 4;
   uint8_t *bits = calloc(width * height * components, sizeof(*bits));
 
-  complex double c = self.c;
-  
+  complex long double c = self.c;
+  long double blowup = self.blowup;
   double scale = 1.5;
-  
+
   for (NSUInteger y = minY; y < maxY; ++y) {
     for (NSUInteger x = minX; x < maxX; ++x) {
       NSUInteger iteration = 0;
-      complex double z = (2.0 * scale * (x-minX))/width - scale
+      complex long double z = (2.0 * scale * (x-minX))/width - scale
         + I*((2.0 * scale * (y - minY))/width - scale);
-      while (cabsf(z) < blowup && iteration < 256) {
+      while (cabsl(z) < blowup && iteration < 256) {
         z = f(z, c);
         ++iteration;
       }
+      
       NSUInteger offset = (y * width * components) + (x * components);
       bits[offset+0] = (iteration * self.rScale);
       bits[offset+1] = (iteration * self.bScale);
       bits[offset+2] = (iteration * self.gScale);
     }
   }
-  
+
   CGColorSpaceRef colorspace = CGColorSpaceCreateDeviceRGB();
   CGContextRef context = CGBitmapContextCreate(bits,
                                                width,
@@ -78,13 +78,14 @@ complex double f(complex double z, complex double c) {
 }
 
 - (void)configureWithSeed:(NSUInteger)seed
-{
+{  
   srandom(seed);
   
-  self.c = (double)random()/LONG_MAX + I*(double)random()/LONG_MAX;
-  self.rScale = random() % 50;  // Biased, but repeatable and soimple is more important
-  self.gScale = random() % 50;
-  self.bScale = random() % 50;
+  self.c = (long double)random()/LONG_MAX + I*(long double)random()/LONG_MAX;
+  self.blowup = random();
+  self.rScale = random() % 20;  // Biased, but repeatable and soimple is more important
+  self.gScale = random() % 20;
+  self.bScale = random() % 20;
   self.label.text = [NSString stringWithFormat:@"(%.3f, %.3f)",
                      creal(self.c), cimag(self.c)];
   [self calculateImage];
