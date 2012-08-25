dc219967fb3cb3ac0501b89c8340da9d549f2c06
Rob Napier <robnapier@gmail.com>
2012-08-24 20:41:27 -0400

    Move Richtext

---
+++ ch26/RichText/RichText/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RichText
+//
+//  Created by Rob Napier on 8/20/12.
+//  Copyright (c) 2012 Rob Napier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
