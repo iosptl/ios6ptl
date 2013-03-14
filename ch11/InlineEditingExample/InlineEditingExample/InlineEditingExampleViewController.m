353ed11ae75f0e1f238467d482a866a6635b6591
martinezdelariva <martinezdelariva@gmail.com>
2013-03-14 20:19:10 +0100

    Fixed number of rows
    Populate current number of rows from data source

--- ch11/InlineEditingExample/InlineEditingExample/InlineEditingExampleViewController.m
+++ ch11/InlineEditingExample/InlineEditingExample/InlineEditingExampleViewController.m
@@ -50,7 +50,7 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     // Return the number of rows in the section.
-    return 15;
+    return [self.data count];
 }
 
 
